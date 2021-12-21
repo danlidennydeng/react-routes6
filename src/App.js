1a3cd9778257b5eda4137c6eb514f852ae03ad16
Danli Denny Deng <danlideng@yahoo.com>
2021-12-21 07:05:16 -0800

    react-router-dom@6 works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
+import React from 'react'  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            {/* <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>SuperHeroes</Link>
+            </li> */}
+            <li>
+              <Link to='/rq-super-heroes'>RQ SuperHeroes</Link>
+            </li>
+
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          {/* <Route path='/'>
+            <HomePage />
+          </Route>
+          <Route exact path='/super-heroes'>
+            <SuperHeroesPage />
+          </Route> */}
+
+          <Route exact path='/rq-super-heroes' element={<RQSuperHeroesPage/>}></Route>
+            
+          <Route path='/about' element={<About />}></Route>
+            
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
